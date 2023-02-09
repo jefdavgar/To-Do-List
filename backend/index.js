fd4e28b4cfe5efa8cc118ec572c45364122c6173
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-02-09 09:22:42 -0500

    Add backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const port = 3030;
+const app = express();
+
+// Importar rutas Todo
+const todoRoutes = require("./routes/todoRoutes");
+
+// Opciones de conexión a MongoDB
+const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true };
+// Usar JSON para parsear el cuerpo de la solicitud
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1018@cluster0.kxt35nb.mongodb.net/todolist",
+    connectionOptions
+  )
+  .then(() => console.log("Connected successfully"))
+  .catch((err) => console.error(err));
+app.use("/todos", todoRoutes);
+app.listen(port, () => {
+  console.log("This server is listening on port: " + port);
+});

63f6038872a23e691cf0cef944c06f79809df26e
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-02-09 14:40:14 -0500

    new index.js without proxy

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const proxy = "https://cors-anywhere.herokuapp.com/http://localhost:3030";
+
 export default axios.create({
-    baseURL: proxy + "http://localhost:3030"
+    baseURL: "http://localhost:3030"
 });
